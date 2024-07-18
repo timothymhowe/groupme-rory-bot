cd54a3d5a8bbb39b55c0e781bbb5e553c8258f56
Timothy Howe <tim@ailette.io>
2024-07-17 23:27:27 -0400

    changed facts, edited some behaviors

--- handle_messages.py
+++ handle_messages.py
@@ -1,21 +1,23 @@
 from random import randint
-from rory import get_random_rory_fact_payload
+import os
+from rory import get_random_rory_fact_payload, get_random_rory_fact
 
-GROUPME_BOT_ID = "aed3cc26b4e763add79c53b3a6"
+GROUPME_BOT_ID = "8c57401d35ebfc04348db5c20e"
 
-def handle_message(message_text,sender_name ="simp"):
+
+def handle_message(message_text, sender_name="simp"):
     payload = ""
-    
+
     if "bryson" in message_text.lower():
         payload = {
-            "bot_id"  : GROUPME_BOT_ID,
-            "attachments" : [
+            "bot_id": GROUPME_BOT_ID,
+            "attachments": [
                 {
-                "type"  : "image",
-                "url"   : "https://i.groupme.com/542x406.png.7867989eb8bb4feab6adecaab94a68f9.large"
+                    "type": "image",
+                    "url": "https://i.groupme.com/542x406.png.7867989eb8bb4feab6adecaab94a68f9.large",
                 }
-            ]
-            } 
+            ],
+        }
     elif "!roryfact" in message_text.lower()[0:12]:
         payload = rory_facts(sender_name)
     elif "rory" in message_text.lower():
@@ -24,83 +26,84 @@ def handle_message(message_text,sender_name ="simp"):
         payload = rory_response(sender_name)
     elif "mcilroy" in message_text.lower():
         payload = rory_response(sender_name)
-        
-    return payload   
+
+    return payload
 
 
-def rory_facts(sender_name:str="simp"):
+def rory_facts(sender_name: str = "simp"):
     payload = get_random_rory_fact_payload(GROUPME_BOT_ID)
     return payload
 
+
 def rory_response(sender_name):
-    
+
     if not sender_name:
         sender_name = "simp"
     payload = ""
-    
-    
-    # choose a random result
-    x = randint(1,4)
-   
 
-    if x==6:
-        pass
-        # payload = {
-        #     'bot_id': GROUPME_BOT_ID,
-        #     'text': f"Hey, {sender_name}. Stop trying to make Rory happen. It's not going to happen."
-        # }
-            
-    elif x==1:
-        
+    # choose a random result
+    x = randint(1, 4)
+
+    last_random_main = get_last_generated_number()
+    new_number = randint(2, 5)
+    while new_number == last_random_main:
+        new_number = randint(2, 5)
+    set_last_generated_number(new_number)
+    x = new_number
+
+    # switch for the different message options.  not great, but not terrible.  
+    if x == 1:
         payload = {
-            "bot_id"  : GROUPME_BOT_ID,
-            "attachments" : [
+            "bot_id": GROUPME_BOT_ID,
+            "attachments": [
                 {
-                "type"  : "image",
-                "url"   : "https://i.groupme.com/542x406.png.7867989eb8bb4feab6adecaab94a68f9.large"
+                    "type": "image",
+                    "url": "https://i.groupme.com/542x406.png.7867989eb8bb4feab6adecaab94a68f9.large",
                 }
-            ]
-            }
-        
-        
-    elif x==7:
-        pass
-        # payload = {
-        #     "bot_id"  : GROUPME_BOT_ID,
-        #     "message" : "Rory addiction is a serious disease that affects millions of golf fans around the world.",
-        #     "attachments" : [
-        #         {
-        #         "type"  : "image",
-        #         "url"   : "https://i.groupme.com/480x358.gif.2d2aa3e614524c2eb5b03437ae927eb0.large"
-        #         }
-        #     ]
-        #     }
-        
-        
-    elif x==2:
+            ],
+        }
+
+    elif x == 2:
         payload = {
-            "bot_id"  : GROUPME_BOT_ID,
-            "attachments" : [
+            "bot_id": GROUPME_BOT_ID,
+            "attachments": [
                 {
-                "type"  : "image",
-                "url"   : "https://i.groupme.com/480x270.gif.9a4b943354ed4796896f2a9699f5360b.large"
+                    "type": "image",
+                    "url": "https://i.groupme.com/480x270.gif.9a4b943354ed4796896f2a9699f5360b.large",
                 }
-            ]
-            }
-         
-    elif x==3:
+            ],
+        }
+
+    elif x == 3:
         payload = {
-            "bot_id"  : GROUPME_BOT_ID,
-            "attachments" : [
+            "bot_id": GROUPME_BOT_ID,
+            "attachments": [
                 {
-                "type"  : "image",
-                "url"   : "https://i.groupme.com/2094x1572.jpeg.726e68e391bd43c48840bed10626481c.large"
+                    "type": "image",
+                    "url": "https://i.groupme.com/2094x1572.jpeg.726e68e391bd43c48840bed10626481c.large",
                 }
-            ]
-            } 
-    
-    elif x==4:
-        payload = get_random_rory_fact_payload(GROUPME_BOT_ID)
-    
-    
+            ],
+        }
+
+    elif x == 4:
+        payload = {
+            "bot_id": GROUPME_BOT_ID,
+            "text": f"Fun Fact: {get_random_rory_fact()}",
+        }
+
+    elif x == 5:
+        payload = {"bot_id": GROUPME_BOT_ID, "text": "cope harder"}
+
     return payload
+
+
+def get_last_generated_number():
+    if os.path.exists("last_random_main.txt"):
+        with open("last_random_main.txt", "r") as file:
+            return int(file.read())
+    return None
+
+
+def set_last_generated_number(number):
+    with open("last_random_main.txt", "w") as file:
+        file.write(str(number))
--- rory.py
+++ rory.py
@@ -1,6 +1,6 @@
 from random import randint
 from datetime import datetime
-
+import os
 
 def get_random_rory_fact_payload(GROUPME_BOT_ID):
     
@@ -23,27 +23,46 @@ def get_random_rory_fact():
 
     # Calculate the difference in days
     days_since_missed_putt = (current_date - last_missed_3_footer_date).days
-    days_since_last_major = (current_date - last_major_date)
+    days_since_last_major = (current_date - last_major_date).days
     facts = [
-        "Rory McIlroy famously blew a four-shot lead during the final round of the 2011 Masters, finishing tied for 15th.",
+        "Rory McIlroy blew a four-shot lead during the final round of the 2011 Masters, finishing tied for 15th.",
         f"It has been {days_since_last_major} day(s) since Rory McIlroy has won a major.",
         f"It has been {days_since_missed_putt} day(s) since Rory McIlroy missed a 3 foot putt in a major.",
         "Rory has missed 2 cuts at The Masters in the last 4 years.",
-        "Rory McIlroy withdrew from the 2013 Honda Classic mid-round citing severe tooth pain, an excuse media and fans alike knew was bullshit.",
+        "Rory McIlroy withdrew from the 2013 Honda Classic mid-round citing severe tooth pain, an excuse recognized as utter bullshit by media and fans alike.",
         "In 2015, McIlroy suffered an ankle injury while playing soccer, causing him to miss the Open Championship.",
         "His 2016 season was affected by a rib injury that hampered his performance and led to several missed tournaments.",
-        "McIlroy faced criticism for his decision to skip the 2016 Olympics in Rio de Janeiro, citing concerns over the Zika virus.",
-        "Rory McIlroy famously missed the cut at the 2019 Open Championship at Royal Portrush, less than an hour from his childhood home.",
+        "Rory skipped the 2016 Olympics in Rio de Janeiro, citing his concerns for the Zika Virus.  This decison was widely regarded as irrational, given that Rory gets absolutely zero box.",
+        "Rory McIlroy missed the cut at the 2019 Open Championship at Royal Portrush, less than an hour from his childhood home.",
         "McIlroy failed to win any tournaments in 2020, breaking a streak of winning at least one event per year for over a decade.",
         "Despite his talent, McIlroy has struggled with consistency in putting, which has been a recurring weakness in his game.",
         "In 2012, McIlroy was penalized for slow play during the European Tour's Abu Dhabi HSBC Golf Championship.",
-        "McIlroy's ex-fiancee and former World No. 1 in Tennis, Caroline Wozniacki, won a Grand Slam as recently as 2018",
+        "McIlroy's ex-fiancee and former World No. 1 in Tennis, Caroline Wozniacki, won a Grand Slam as recently as 2018.",
       
-        'In the 125th Open Championship, Rory McIlroy failed to hole a putt longer 4\'9" in the final round.  In the final round of the 2024 US Open, he failed twice to make putts shorter than 4". Brutal.',
-        "McIlroy fired the caddy he won 4 majors with because he had the audacity to question Rory's lack of focus and preparation after bogeying 5 of the first 6 holes in the first round of the 2017 Open Championship at Birkdale.",
+        'In the 125th Open Championship, Rory McIlroy failed to hole a putt longer 4\'9" in the final round.  In the final 5 holes of the 2024 US Open, he failed twice to make putts shorter than 4". Tough.',
+        "In 2017, McIlroy fired caddie JP Fitzgerald--with whom he had won 4 majors--because Fitzgerald had the audacity to comment on Rory's lack of focus after bogeying 5 of the first 6 holes in the first round of the Open Championshop.",
         'Rory McIlroy is one of the Tour\'s longest drivers, despite standing at just 5\'4" tall.'
     ]
 
-    return facts[randint(0,len(facts)-1)]
+    last_rory_fact = get_last_generated_number()
+    new_number = randint(0,len(facts)-1)
+    while new_number==last_rory_fact:
+       new_number = randint(0,len(facts)-1) 
+
+
+    set_last_generated_number(new_number)
+
+    return facts[new_number]
+
+
+def get_last_generated_number():
+    if os.path.exists('last_rory_fact.txt'):
+        with open('last_rory_fact.txt', 'r') as file:
+            return int(file.read())
+    return None
+
+def set_last_generated_number(number):
+    with open('last_rory_fact.txt', 'w') as file:
+        file.write(str(number))
 
 
